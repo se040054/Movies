311334303af207de56c16ebbb38fd8c64dd35e83
se040054 <peter8822967@gmail.com>
2023-07-05 22:09:22 +0800

    build: use express-handlebars as view engine 加入樣板引擎 express handlebar

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
 const app=express()
 const port =3000 ;
 
 
 app.use(express.static('public'))
 
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine','.hbs')
+app.set('views','./views')
+
 app.get('/',(req,res) => {
   res.redirect('/movies')
+  
 })
 
 app.get('/movies',(req,res) =>{
-  res.send('listing movies')
+  res.render('index');
 })
 
 app.get('/movie/:id',(req,res)=>{
